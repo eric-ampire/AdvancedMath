91d7c743fae47d548895e4b4e20b8967faf7a426
Eric Ampire <eric.ampire@brp.com>
2024-04-03 10:32:42 -0400

    Initial Commit

---
+++ AdvancedMath/AdvancedMath.h
@@ -0,0 +1,18 @@
+//
+//  AdvancedMath.h
+//  AdvancedMath
+//
+//  Created by Eric Ampire (Admin) on 2024-04-03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdvancedMath.
+FOUNDATION_EXPORT double AdvancedMathVersionNumber;
+
+//! Project version string for AdvancedMath.
+FOUNDATION_EXPORT const unsigned char AdvancedMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdvancedMath/PublicHeader.h>
+
+
